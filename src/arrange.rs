f48d4b3e52e3728e4797e911e899968c65f725d3
Rust & Python <nexinov@localhost.gud-o15>
2021-06-26 14:12:07 +0530

    Formatted the code

--- src/arrange.rs
+++ src/arrange.rs
@@ -1,72 +1,81 @@
 // Sorting
 
 // Importing std
-use std::io::Read;
 use std::fs::OpenOptions;
+use std::io::Read;
 use std::io::Write;
 
-fn main() { //Main
-	let file = std::env::args().nth(1).expect("Give a file to sort!"); //Getting file to sort
-	//Making Ascending and descending.csv files.
-	make_file("docs/ascending.csv".to_string()); 
-	make_file("docs/descending.csv".to_string());
-	read(file); //reading file
-	println!("Have a look at docs/ascending.csv and/or docs/descending.csv!"); //After all
+fn main() {
+    //Main
+    let file = std::env::args().nth(1).expect("Give a file to sort!"); //Getting file to sort
+                                                                       //Making Ascending and descending.csv files.
+    make_file("docs/ascending.csv".to_string());
+    make_file("docs/descending.csv".to_string());
+    read(file); //reading file
+    println!("Have a look at docs/ascending.csv and/or docs/descending.csv!"); //After all
 }
 
 fn read(filename: String) {
-	let mut file = std::fs::File::open(&filename).unwrap(); //Getting file and unwrapping
-	let mut contents = String::new(); //Making a string for contents
-	file
-		.read_to_string(&mut contents) //reading
-		.unwrap();
-	let contents = &contents.to_string(); //Converting to string(as it is stored in f64 format).
+    let mut file = std::fs::File::open(&filename).unwrap(); //Getting file and unwrapping
+    let mut contents = String::new(); //Making a string for contents
+    file.read_to_string(&mut contents) //reading
+        .unwrap();
+    let contents = &contents.to_string(); //Converting to string(as it is stored in f64 format).
 
-	let split = contents.split(", "); //splitting the contents
-	let vec: Vec<&str> = split.collect(); //collecting them
-	let mut vect = vec![]; //vect1 (for f64 val) [ascending]
-	let mut vect2 = vec![]; //vector2 (for String val) [ascending]
-	let mut vect4: Vec<String> = vec![]; // for descending
+    let split = contents.split(", "); //splitting the contents
+    let vec: Vec<&str> = split.collect(); //collecting them
+    let mut vect = vec![]; //vect1 (for f64 val) [ascending]
+    let mut vect2 = vec![]; //vector2 (for String val) [ascending]
+    let mut vect4: Vec<String> = vec![]; // for descending
 
-	for num in 0..vec.len() { //Adding to vector
-		let fnum: f64 = vec[num].parse().unwrap(); //collecting the numbers seperately
-		vect.push(fnum); //pushing to vector
-	}
+    for num in 0..vec.len() {
+        //Adding to vector
+        let fnum: f64 = vec[num].parse().unwrap(); //collecting the numbers seperately
+        vect.push(fnum); //pushing to vector
+    }
 
-	vect.sort_by(|a, b| a.partial_cmp(b).unwrap()); //Sorting
+    vect.sort_by(|a, b| a.partial_cmp(b).unwrap()); //Sorting
 
-	for num in 0..vect.len() { //for loop [for String]
-		let a_num = vect[num].to_string(); //conversion to string
-		vect2.push(a_num); //pushing to vector
-	}
+    for num in 0..vect.len() {
+        //for loop [for String]
+        let a_num = vect[num].to_string(); //conversion to string
+        vect2.push(a_num); //pushing to vector
+    }
 
-	let vect3: Vec<f64> = vect.into_iter().rev().collect(); //reversing the vec [for descending]
+    let vect3: Vec<f64> = vect.into_iter().rev().collect(); //reversing the vec [for descending]
 
-	for num in 0..vect3.len() { //for loop [for descending]
-		let ab = vect3[num].to_string(); //converting to string
-		vect4.push(ab); //pushing
-	}
+    for num in 0..vect3.len() {
+        //for loop [for descending]
+        let ab = vect3[num].to_string(); //converting to string
+        vect4.push(ab); //pushing
+    }
 
-	let vect2 = vect2.join(", "); //joining as string
-	let vect4 = vect4.join(", "); //joining as string
-	write_to_file("docs/ascending.csv".to_string(), vect2); //Writing
-	write_to_file("docs/descending.csv".to_string(), vect4); //Writing
+    let vect2 = vect2.join(", "); //joining as string
+    let vect4 = vect4.join(", "); //joining as string
+    write_to_file("docs/ascending.csv".to_string(), vect2); //Writing
+    write_to_file("docs/descending.csv".to_string(), vect4); //Writing
 }
 
-fn make_file(file_name: String) { //Making file (same as in main.rs)
+fn make_file(file_name: String) {
+    //Making file (same as in main.rs)
     std::fs::File::create(&file_name).expect("\nCreation failed");
-    println!("\n\nCreated file named {} \nPATH: docs/{}\n", &file_name, &file_name);
+    println!(
+        "\n\nCreated file named {} \nPATH: docs/{}\n",
+        &file_name, &file_name
+    );
 }
 
 fn write_to_file(file: String, text: String) {
-	let mut file = OpenOptions::new() //Opening
-		.append(true) //Appening
-		.open(&file)
-		.expect("\nCannot Open File."); //Error response
+    let mut file = OpenOptions::new() //Opening
+        .append(true) //Appening
+        .open(&file)
+        .expect("\nCannot Open File."); //Error response
 
-	file
-		.write_all(text.as_bytes()) //writing as bytes
-		.expect("\nWrite Failed"); //Error response
+    file.write_all(text.as_bytes()) //writing as bytes
+        .expect("\nWrite Failed"); //Error response
 
-	println!("Successfully appended sorted list of numbers to file {:?}.", file); //printing ans(for user)
+    println!(
+        "Successfully appended sorted list of numbers to file {:?}.",
+        file
+    ); //printing ans(for user)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,34 @@
 // A simple code to make 'x' amount of random numbers(floats).
 
 // Importing rand, and std.
-use rand::Rng; 
+use rand::Rng;
 use std::fs::OpenOptions;
 use std::io::Write;
 
-fn main() { //Main
-	let file = std::env::args().nth(1).expect("Give File"); // Getting file name.
-	let numofnums: i32 = std::env::args() 
-		.nth(2) //Second value
-		.expect("Give Num - only i32") //Error response
-		.parse() 
-		.unwrap(); //Converting to i32
+fn main() {
+    //Main
+    let file = std::env::args().nth(1).expect("Give File"); // Getting file name.
+    let numofnums: i32 = std::env::args()
+        .nth(2) //Second value
+        .expect("Give Num - only i32") //Error response
+        .parse()
+        .unwrap(); //Converting to i32
 
-	let mut rng = rand::thread_rng(); //making rng to get random nums
-	let mut nums = vec![]; //Making an empty vector to store values.
+    let mut rng = rand::thread_rng(); //making rng to get random nums
+    let mut nums = vec![]; //Making an empty vector to store values.
 
-	// for loop to make 'numofnums' rand nums
-	for _num in 0..numofnums {
-		let num = rng.gen_range(0.1..1000.0); //Generating
-		let num = num.to_string(); //Converting nums to string (to add to file)
-		nums.push(num); //Pushing to vector
-	}
-	
-	let fnums = nums.join(",\n"); //Joining the vector with ',' and '\n'(newline) to convert the vector to string.
-	println!("{} numbers", nums.len()); //Printing the number of numbers. ;-)
+    // for loop to make 'numofnums' rand nums
+    for _num in 0..numofnums {
+        let num = rng.gen_range(0.1..1000.0); //Generating
+        let num = num.to_string(); //Converting nums to string (to add to file)
+        nums.push(num); //Pushing to vector
+    }
 
-	create_file(file.to_string()); //Creating file
-	write_to_file(file.to_string(), fnums, numofnums); //Writing to file
+    let fnums = nums.join(",\n"); //Joining the vector with ',' and '\n'(newline) to convert the vector to string.
+    println!("{} numbers", nums.len()); //Printing the number of numbers. ;-)
+
+    create_file(file.to_string()); //Creating file
+    write_to_file(file.to_string(), fnums, numofnums); //Writing to file
 }
 
 fn create_file(file_name: String) {
@@ -36,14 +37,16 @@ fn create_file(file_name: String) {
 }
 
 fn write_to_file(file_name: String, text: String, numofnums: i32) {
-	let mut file = OpenOptions::new()
-		.append(true) //Appending
-		.open(&file_name) 
-		.expect("\nCannot Open File."); //Error response
+    let mut file = OpenOptions::new()
+        .append(true) //Appending
+        .open(&file_name)
+        .expect("\nCannot Open File."); //Error response
 
-	file
-		.write_all(text.as_bytes()) //As bytes 
-		.expect("\nWrite Failed"); //Error response
+    file.write_all(text.as_bytes()) //As bytes
+        .expect("\nWrite Failed"); //Error response
 
-	println!("Successfully appended {} random numbers to file {}.", numofnums, file_name); //Printing confirmation line.
+    println!(
+        "Successfully appended {} random numbers to file {}.",
+        numofnums, file_name
+    ); //Printing confirmation line.
 }
